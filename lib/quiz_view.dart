2452a5ccbd7cade8a6addb7323cd2c4b0b20477c
Mehmet Emin GÜler <mhmt.eminguler@gmail.com>
2025-03-18 15:36:53 +0100

    Remove unused image assets and update quiz view logic to display dynamic questions and options

--- lib/quiz_view.dart
+++ lib/quiz_view.dart
@@ -1,3 +1,7 @@
+import 'dart:collection';
+
+import 'package:flag_quiz_app/flags.dart';
+import 'package:flag_quiz_app/flagsdao.dart';
 import 'package:flag_quiz_app/result_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,73 @@ class QuizScreenView extends StatefulWidget {
 }
 
 class _QuizScreenViewState extends State<QuizScreenView> {
+  var questions = <Flags>[];
+  var wrongOptions = <Flags>[];
+  late Flags currentQuestion;
+  var allOptions = HashSet<Flags>();
+
+  int questionCounter = 0;
+  int correctCounter = 0;
+  int wrongCounter = 0;
+
+  String flagImageName = "placeholder.png";
+
+  String optionA = "";
+  String optionB = "";
+  String optionC = "";
+  String optionD = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getQuestion();
+  }
+
+  Future<void> getQuestion() async {
+    questions = await Flagsdao().getRandom5Flags();
+    loadQuestion();
+  }
+
+  Future<void> loadQuestion() async {
+    currentQuestion = questions[questionCounter];
+    flagImageName = currentQuestion.flag_image;
+
+    allOptions.clear();
+    allOptions.add(currentQuestion);
+    allOptions.add(wrongOptions[0]);
+    allOptions.add(wrongOptions[1]);
+    allOptions.add(wrongOptions[2]);
+
+    optionA = allOptions.elementAt(0).flag_name;
+    optionB = allOptions.elementAt(1).flag_name;
+    optionC = allOptions.elementAt(2).flag_name;
+    optionD = allOptions.elementAt(3).flag_name;
+
+    setState(() {});
+  }
+
+  void questionCounterCheck() {
+    if (questionCounter < 4) {
+      questionCounter++;
+      loadQuestion();
+    } else {
+      Navigator.pushReplacement(
+          context,
+          CupertinoPageRoute(
+              builder: (context) => ResultScreenView(
+                    correct: correctCounter,
+                  )));
+    }
+  }
+
+  void resultCheck(String buttonName) {
+    if (buttonName == currentQuestion.flag_name) {
+      correctCounter++;
+    } else {
+      wrongCounter++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,25 +95,16 @@ class _QuizScreenViewState extends State<QuizScreenView> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Correct: 0"),
-                Text("Wrong: 0"),
+                Text("Correct: $correctCounter"),
+                Text("Wrong: $wrongCounter"),
               ],
             ),
-            Text("Qestion 1"),
-            Image.asset("assets/turkiye.png"),
-            FilledButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                      context,
-                      CupertinoPageRoute(
-                          builder: (context) => ResultScreenView(
-                                correct: 3,
-                              )));
-                },
-                child: Text("Button A")),
-            FilledButton(onPressed: () {}, child: Text("Button B")),
-            FilledButton(onPressed: () {}, child: Text("Button C")),
-            FilledButton(onPressed: () {}, child: Text("Button D")),
+            Text("Qestion ${questionCounter + 1}"),
+            Image.asset("assets/$flagImageName"),
+            FilledButton(onPressed: () {}, child: Text("$optionA")),
+            FilledButton(onPressed: () {}, child: Text(" $optionB")),
+            FilledButton(onPressed: () {}, child: Text("$optionC")),
+            FilledButton(onPressed: () {}, child: Text("$optionD")),
           ],
         ),
       ),
